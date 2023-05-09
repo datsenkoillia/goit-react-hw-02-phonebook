649fa4d3495f4806c69a4bade40bbe91d8196744
Illia Datsenko <datsenko.i@gmail.com>
2023-05-09 19:50:07 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { InputLabel, InputField, SubmitButton, Form } from './styled';
 
@@ -52,3 +53,7 @@ export class ContactForm extends Component {
     );
   }
 }
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
